5d085ca4d72c7bf3237cf61bde317f3d738831f9
Darina <darynadrobot9@gmail.com>
2026-08-18 16:58:10 +0200

    Update hw4_1.py

--- hw4_1.py
+++ hw4_1.py
@@ -8,7 +8,6 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-    # реалізація класу
 		pass
 
 class Phone(Field):
@@ -19,52 +18,53 @@ class Phone(Field):
 		
 
 class Record:
+
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
-    def add_phone(self, phone_number):
-        phone = Phone(phone_number)
-        self.phones.append(phone)
-    def find_phone(self, phone_number):
-        for phone in self.phones:
-            if phone.value == phone_number:
-                return phone  
+
+    def add_phone(self, phone):
+        phone_obj = Phone(phone)
+        self.phones.append(phone_obj)
+
+    def find_phone(self, phone):
+        for phone_obj in self.phones:
+            if phone_obj.value == phone:
+                return phone_obj
         return None
-    def remove_phone(self, phone_number):
-        phone = self.find_phone(phone_number)  
-        if phone:
-            self.phones.remove(phone)
+
+    def remove_phone(self, phone):
+        phone_obj = self.find_phone(phone)
+        if phone_obj:
+            self.phones.remove(phone_obj)
 
     def edit_phone(self, old_phone, new_phone):
-        phone = self.find_phone(old_phone)
-        if not phone:
-            raise ValueError("Старый номер не знайдено")
+        phone_obj = self.find_phone(old_phone)
+        if not phone_obj:
+            raise ValueError("Старий номер телефону не знайдено")
+
         new_phone_obj = Phone(new_phone)
-        phone.value = new_phone_obj.value
+        index = self.phones.index(phone_obj)
+        self.phones[index] = new_phone_obj
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-        
-                  
-                
 
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
-    def add_record(self, record):
-            self.data[record.name.value]= record
 
+    def add_record(self, record):
+        self.data[record.name.value] = record
 
     def find(self, name):
         return self.data.get(name)
 
-
     def delete(self, name):
         if name in self.data:
             del self.data[name]
-              
-              
+
+    def __str__(self):
+        return "\n".join(str(record) for record in self.data.values())
     
                     
                    
